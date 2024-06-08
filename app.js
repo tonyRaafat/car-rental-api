4f75ca0290b9154ea7b98f23e5e857e0d368aae1
tonyRaafat <tonyrafat01@gmail.com>
2024-06-08 22:05:35 +0300

    feat: add routers to main app

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 import express from 'express'
-import { db } from './db/dbConnection.js'
+import customerRouter from './modules/customers/customers.routers.js'
+import carRouter from './modules/Cars/cars.routers.js'
+import rentalRouter from './modules/rentals/rentals.routers.js'
+
+import errorHandler from './utils/errorHandler.utils.js'
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.json())
+app.use('/customer',customerRouter)
+app.use('/car',carRouter)
+app.use('/rentals',rentalRouter)
+
+
+app.all('*', (req, res, next) => {
+    const error = new Error(`Cannot ${req.method} ${req.originalUrl}`)
+    error.statusCode = 404
+    next(error)
+})
+
+app.use(errorHandler)
+
 app.listen(port, () => console.log(`app listening on port ${port}`))
